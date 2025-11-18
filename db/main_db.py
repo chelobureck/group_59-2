9756950b000b4310327a02095ea7f4da022cc4f7
chelobureck <adylbekoveldiyar@gmail.com>
2025-11-18 20:47:39 +0600

    урок 7

--- db/main_db.py
+++ db/main_db.py
@@ -21,18 +21,31 @@ def add_task(task: str | None, time: str) -> int:
     conn.close()
     return task_id
 
-def get_tasks() -> list:
+def get_tasks(filter_type: str) -> list:
     conn = sqlite3.connect(path_db)
     cursor = conn.cursor()
-    cursor.execute(querles.SELECT_TASK)
+
+    if filter_type == "completed":
+        cursor.execute(querles.SELECT_COMPLETED_TASK)
+    elif filter_type == "uncompleted":
+        cursor.execute(querles.SELECT_UNCOMPLETED_TASK)
+    else:
+        cursor.execute(querles.SELECT_TASK)
+
     tasks = cursor.fetchall()
     conn.close()
     return tasks
 
-def update_task(id: int, task: str | None, time: str | None) -> None:
+def update_task(id: int, task: str | None = None , completed: int | None = None, time: str | None = None) -> None:
     conn = sqlite3.connect(path_db)
     cursor = conn.cursor()
-    cursor.execute(querles.UPDATE_TASK, (task, time, id))
+
+    if task is not None:
+        cursor.execute(querles.UPDATE_TASK, (task, time, id))
+
+    if completed is not None:
+        cursor.execute("UPDATE tasks SET completed = ? WHERE id = ?", (completed, id))
+    
     conn.commit()
     conn.close()
 
